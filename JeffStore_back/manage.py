d9c9ba82b7c1fdf63217d9f1576e21f755c6edac
LXJ <lixiaojie@itssoft.net>
2019-09-02 20:30:26 +0800

    增加前端文件

--- JeffStore_back/manage.py
+++ JeffStore_back/manage.py
@@ -3,6 +3,12 @@
 __author__: jack
 __date__:2019/9/1 14:27
 """
+import sys
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# 添加导包路径
+sys.path.insert(0, os.path.join(BASE_DIR, 'apps'))
+
 from apps import create_app
 
 
